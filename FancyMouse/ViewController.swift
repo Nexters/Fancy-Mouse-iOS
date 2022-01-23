59ea6f63696f75f5d7555075016e638a81b66cfe
Lee424 <tmdghks424@naver.com>
2022-01-23 18:54:02 +0900

    Init Project

---
+++ FancyMouse/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  FancyMouse
+//
+//  Created by seunghwan Lee on 2022/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ FancyMouseTests/FancyMouseTests.swift
@@ -0,0 +1,33 @@
+//
+//  FancyMouseTests.swift
+//  FancyMouseTests
+//
+//  Created by seunghwan Lee on 2022/01/22.
+//
+
+import XCTest
+@testable import FancyMouse
+
+class FancyMouseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
