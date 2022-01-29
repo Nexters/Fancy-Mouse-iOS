1c6c6a865093c5ecd83a499c48842b3cb6e38e0a
yeonduing <yeonduing@gmail.com>
2022-01-29 23:06:50 +0900

    Word Entity 생성

--- FancyMouse/Domain/Entities/Word.swift
+++ FancyMouse/Domain/Entities/Word.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct Word {
+    let spelling: String
+    let meaning: String
+    let createdAt: Date
+    let memo: String
+    let status: Status
+    
+    enum Status {
+        case ready, inProgress, fininshed
+        
+        var description: String {
+            switch self {
+            case .ready: return "미암기"
+            case .inProgress: return "암기중"
+            case .fininshed: return "암기완료"
+            }
+        }
+    }
+}
