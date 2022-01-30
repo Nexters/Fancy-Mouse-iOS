19d3bb5c4f6280d3f7056ec69a55f697bc02d383
yeonduing <yeonduing@gmail.com>
2022-01-30 15:11:39 +0900

    Home 관련 UseCase 정리

--- FancyMouse/Domain/UseCases/HomeUseCases.swift
+++ FancyMouse/Domain/UseCases/HomeUseCases.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+protocol WordUseCase {
+    func changeStatus(to newStatus: Word.Status)
+}
+
+protocol WordTestUseCase: WordUseCase {
+    func hideSpelling()
+    func hideMeaning()
+    func suffleWords()
+}
+
+protocol WordEditUseCase {
+    // TODO: 폴더 변경을 어떻게 풀지 논의 필요
+    func moveFolder(to folder: Folder)
+    func editMemo(_ memo: String)
+}
+
+protocol WordDeleteUseCase {
+    func deleteWord()
+}
+
+protocol RemoteWordEditUseCase: WordEditUseCase, WordDeleteUseCase { }
+
+protocol CustomWordEditUseCase: WordEditUseCase {
+    func editWord(_ word: String)
+    func editMeaning(_ meaning: String)
+}
