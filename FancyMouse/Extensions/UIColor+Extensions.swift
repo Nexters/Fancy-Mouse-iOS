ba6907bcd47763a4e3a63a1ef3f6f826bda0c141
Lee424 <tmdghks424@naver.com>
2022-02-07 01:12:10 +0900

    [Update] extensions

--- FancyMouse/Extensions/UIColor+Extensions.swift
+++ FancyMouse/Extensions/UIColor+Extensions.swift
@@ -5,4 +5,34 @@
 //  Created by seunghwan Lee on 2022/02/06.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    static let gray10 = UIColor(named: "gray10")
+    static let gray20 = UIColor(named: "gray20")
+    static let gray30 = UIColor(named: "gray30")
+    static let gray40 = UIColor(named: "gray40")
+    static let gray50 = UIColor(named: "gray50")
+    static let gray60 = UIColor(named: "gray60")
+    static let gray70 = UIColor(named: "gray70")
+    static let gray80 = UIColor(named: "gray80")
+    static let gray90 = UIColor(named: "gray90")
+    static let folder00 = UIColor(named: "folder00")
+    static let folder01 = UIColor(named: "folder01")
+    static let folder02 = UIColor(named: "folder02")
+    static let folder03 = UIColor(named: "folder03")
+    static let folder04 = UIColor(named: "folder04")
+    static let folder05 = UIColor(named: "folder05")
+    static let folder06 = UIColor(named: "folder06")
+    static let folder07 = UIColor(named: "folder07")
+    static let folder08 = UIColor(named: "folder08")
+    static let folder09 = UIColor(named: "folder09")
+    static let folder10 = UIColor(named: "folder10")
+    static let folder11 = UIColor(named: "folder11")
+    static let primary = UIColor(named: "primary")
+    static let primaryWeek = UIColor(named: "primary_week")
+    static let primaryDark = UIColor(named: "primary_dark")
+    static let secondary = UIColor(named: "secondary")
+    static let secondaryWeek = UIColor(named: "secondary_week")
+    static let secondaryDark = UIColor(named: "secondary_dark")
+}
