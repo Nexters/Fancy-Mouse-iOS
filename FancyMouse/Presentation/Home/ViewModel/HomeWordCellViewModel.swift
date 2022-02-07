695da2ec3984d58a7e17c66e348f0b1047daeb7c
yeonduing <yeonduing@gmail.com>
2022-02-07 21:07:42 +0900

    HomeWordCellViewModel 생성

--- FancyMouse/Presentation/Home/ViewModel/HomeWordCellViewModel.swift
+++ FancyMouse/Presentation/Home/ViewModel/HomeWordCellViewModel.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+import RxRelay
+import RxSwift
+
+final class HomeWordCellViewModel {
+    private let useCase: HomeWordUseCaseProtocol
+    
+    private let wordRelay: BehaviorRelay<Word>
+    private let isSpellingHiddenRelay: BehaviorRelay<Bool> = .init(value: false)
+    private let isMeaningHiddenRelay: BehaviorRelay<Bool> = .init(value: false)
+    
+    private let disposeBag = DisposeBag()
+    
+    init(useCase: HomeWordUseCaseProtocol, word: Word) {
+        self.useCase = useCase
+        wordRelay = .init(value: word)
+    }
+    
+    func toggleSpellingHiddenState() {
+        isSpellingHiddenRelay
+            .accept(!isSpellingHiddenRelay.value)
+    }
+    
+    func toggleMeaningHiddenState() {
+        isMeaningHiddenRelay
+            .accept(!isMeaningHiddenRelay.value)
+    }
+    
+    func changeStatus(to status: Word.Status, of wordID: WordID) {
+        useCase.changeStatus(to: status, of: wordID)
+            .subscribe(onNext: { [weak self] in
+                self?.wordRelay.accept($0)
+            })
+            .disposed(by: disposeBag)
+    }
+}
+
+extension HomeWordCellViewModel {
+    var isSpellingHiddenObservable: Observable<Bool> {
+        isSpellingHiddenRelay.asObservable()
+    }
+    
+    var isMeaningHiddenObservable: Observable<Bool> {
+        isMeaningHiddenRelay.asObservable()
+    }
+    
+    var wordObservable: Observable<Word> {
+        wordRelay.asObservable()
+    }
+}
+
