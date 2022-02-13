30bae59027b3693206372ab33743744659316eee
Lee424 <tmdghks424@naver.com>
2022-02-13 21:28:34 +0900

    [Feat] 학습하기 1차

--- FancyMouse/Extensions/Color+Extensions.swift
+++ FancyMouse/Extensions/Color+Extensions.swift
@@ -5,4 +5,15 @@
 //  Created by seunghwan Lee on 2022/02/13.
 //
 
-import Foundation
+import SwiftUI
+
+extension Color {
+    static let gray50 = Color("gray50")
+    static let gray60 = Color("gray60")
+    static let gray70 = Color("gray70")
+    static let primary = Color("primary")
+    static let primaryDark = Color("primary_dark")
+    static let secondary = Color("secondary")
+    static let folder02 = Color("folder02")
+    static let folder07 = Color("folder07")
+}
--- FancyMouse/Presentation/Learning/View/LearningView.swift
+++ FancyMouse/Presentation/Learning/View/LearningView.swift
@@ -5,4 +5,81 @@
 //  Created by seunghwan Lee on 2022/02/13.
 //
 
-import Foundation
+import SwiftUI
+
+struct LearningView: View {
+    @State var checkIsTapped = false
+    @State var xmarkIsTapped = false
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 0) {
+            // TODO: 카드뷰 개발 중, 개발 완료후 교체 예정
+            Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(30)
+                .padding(EdgeInsets(top: 30, leading: 24, bottom: 36, trailing: 24))
+            
+            HStack {
+                HStack(spacing: 5) {
+                    Image("check")
+                        .frame(width: 13, height: 16)
+                        .padding(EdgeInsets(top: 16, leading: 16, bottom: 16, trailing: checkIsTapped ? 8 : 16))
+                    if checkIsTapped {
+                        Text("암기완료")
+                            .spoqaBold(size: 16)
+                            .padding(.trailing, 16)
+                    }
+                }
+                .background(Color.folder02)
+                .foregroundColor(.white)
+                .cornerRadius(16)
+                .onTapGesture {
+                    withAnimation(.interactiveSpring()) {
+                        checkIsTapped.toggle()
+                    }
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                        withAnimation(.interactiveSpring()) {
+                            checkIsTapped.toggle()
+                        }
+                    }
+                }
+                
+                Spacer()
+                
+                HStack(spacing: 5) {
+                    if xmarkIsTapped {
+                        Text("미암기")
+                            .spoqaBold(size: 16)
+                            .padding(.leading, 16)
+                    }
+                    
+                    Image("xmark")
+                        .frame(width: 16, height: 16)
+                        .padding(EdgeInsets(top: 16, leading: xmarkIsTapped ? 8 : 16, bottom: 16, trailing: 16))
+                }
+                .background(Color.folder07)
+                .foregroundColor(.white)
+                .cornerRadius(16)
+                .onTapGesture {
+                    withAnimation(.interactiveSpring()) {
+                        xmarkIsTapped.toggle()
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                        withAnimation(.interactiveSpring()) {
+                            xmarkIsTapped.toggle()
+                        }
+                    }
+                }
+            }
+            .padding(EdgeInsets(top: 0, leading: 60, bottom: 24, trailing: 60))
+        }
+        .background(Color.primaryDark)
+    }
+}
+
+struct LearningView_Previews: PreviewProvider {
+    static var previews: some View {
+        LearningView()
+    }
+}
