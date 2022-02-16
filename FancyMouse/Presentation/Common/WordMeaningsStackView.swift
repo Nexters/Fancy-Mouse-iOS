3243e78bcc338cf535f921f72fdfc26a8b74cd6e
yeonduing <yeonduing@gmail.com>
2022-02-16 22:04:31 +0900

    WordMeaningsStackView 작성

--- FancyMouse/Presentation/Common/WordMeaningsStackView.swift
+++ FancyMouse/Presentation/Common/WordMeaningsStackView.swift
@@ -5,4 +5,21 @@
 //  Created by itzel.du on 2022/02/16.
 //
 
-import Foundation
+import UIKit
+
+final class WordMeaningsStackView: UIStackView {
+    func addSubMeaningViews(with meanings: [String]) {
+        meanings.enumerated().forEach { index, meaning in
+            addSubMeaningView(index: index + 1, meaning: meaning)
+        }
+    }
+    
+    private func addSubMeaningView(index: Int, meaning: String) {
+        let meaningView = WordMeaningView()
+        meaningView.index = index
+        meaningView.meaning = meaning
+        meaningView.translatesAutoresizingMaskIntoConstraints = false
+        
+        addArrangedSubview(meaningView)
+    }
+}
