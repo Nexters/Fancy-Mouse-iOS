abead2baf87e87cc73b2fd3f7ecc28e355e6e3a7
suding <betty.sujin.lee@gmail.com>
2022-02-17 22:06:54 +0900

    [Add] 폴더 화면 UI 구현

--- FancyMouse/Presentation/Folder/View/FolderCell.swift
+++ FancyMouse/Presentation/Folder/View/FolderCell.swift
@@ -1,8 +1,99 @@
 //
-//  FolderCell.swift
-//  FancyMouse
-//
-//  Created by suding on 2022/02/12.
+//  Created by suding on 2022/02/07.
 //
 
-import Foundation
+import UIKit
+import RxCocoa
+import RxSwift
+
+class FolderCell: UICollectionViewCell {
+    static let identifier = "FolderCell"
+    private let disposeBag = DisposeBag()
+    private var currentFolderNum: Int = 0
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.cellSetting()
+        self.setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func cellSetting() {
+        self.backgroundColor = UIColor.white
+        self.addSubview(imageView)
+        imageView.contentMode = .scaleToFill
+        imageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        imageView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        imageView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+
+    var imageView: UIImageView = {
+        let img = UIImageView()
+        img.translatesAutoresizingMaskIntoConstraints = false
+        return img
+    }()
+    
+    var folderImage: UIImageView = {
+        let folderImage = UIImageView()
+        folderImage.image = UIImage(named: "FolderImage")
+        folderImage.translatesAutoresizingMaskIntoConstraints = false
+        return folderImage
+    }()
+    
+    var moreImage: UIImageView = {
+        let listImage = UIImageView()
+        listImage.image = UIImage(named: "more")
+        listImage.translatesAutoresizingMaskIntoConstraints = false
+        return listImage
+    }()
+    
+    var folderName: UILabel = {
+        let folderName = UILabel()
+        folderName.text = "폴더1"
+        folderName.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        folderName.textColor = .darkGray
+        folderName.translatesAutoresizingMaskIntoConstraints = false
+        return folderName
+    }()
+    
+    var wordCount: UILabel = {
+        let wordCount = UILabel()
+        wordCount.text = "32"
+        wordCount.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        wordCount.textColor = .darkGray
+        wordCount.translatesAutoresizingMaskIntoConstraints = false
+        return wordCount
+    }()
+    
+    private func setupUI() {
+        self.addSubview(folderName)
+        folderName.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(48)
+            make.leading.equalToSuperview().offset(24)
+        }
+        
+        self.addSubview(wordCount)
+        wordCount.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(108)
+            make.leading.equalToSuperview().offset(24)
+        }
+        
+        self.addSubview(folderImage)
+        folderImage.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(20)
+            make.leading.equalToSuperview().offset(24)
+        }
+        
+        self.addSubview(moreImage)
+        moreImage.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(21.5)
+            make.trailing.equalToSuperview().offset(-20.5)
+        }
+    }
+    
+    
+}
--- FancyMouse/Presentation/Folder/View/FolderViewController.swift
+++ FancyMouse/Presentation/Folder/View/FolderViewController.swift
@@ -2,7 +2,198 @@
 //  FolderViewController.swift
 //  FancyMouse
 //
-//  Created by suding on 2022/02/12.
+//  Created by suding on 2022/02/11.
 //
 
-import Foundation
+import RxCocoa
+import RxSwift
+import SnapKit
+import UIKit
+
+final class FolderViewController: UIViewController {
+    var viewModel: FolderViewModel?
+    private var folderCount = 12
+    private var disposeBag = DisposeBag()
+    
+    // MARK: dummy data
+    private let folderNameList = [ "폴더1", "수능영어", "토익단어", "TOEFL"]
+    private var webTempString = "www.naver.com"
+    
+    private lazy var emojiView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = 24
+        return view
+    }()
+    
+    private lazy var countLabel: UILabel = {
+        let label = UILabel()
+        label.text = "\(folderCount)"
+        label.font = label.font.withSize(20)
+        label.textColor = .white
+        return label
+    }()
+    
+    private lazy var countExplainLabel: UILabel = {
+        let label = UILabel()
+        label.text = "개의 폴더"
+        label.font = label.font.withSize(18)
+        label.textColor = .white
+        return label
+    }()
+
+    private lazy var webExplainLabel: UILabel = {
+        let label = UILabel()
+        label.text = "단어는 웹에서 등록할 수 있어요!"
+        label.font = label.font.withSize(12)
+        label.textColor = UIColor.gray40
+        return label
+    }()
+    
+    private lazy var pasteImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "ic_copy")
+        return image
+    }()
+    
+    private lazy var pasteLabel: UILabel = {
+        let label = UILabel()
+        label.text = "링크 복사하기"
+        label.font = label.font.withSize(12)
+        label.textColor = UIColor.secondaryDark
+        return label
+    }()
+    
+    private lazy var pasteButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    private lazy var folderImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "Folder")
+        return image
+    }()
+    
+    private var folderCollectionView : UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 12
+        layout.scrollDirection = .vertical
+        layout.sectionInset = .zero
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initSetting()
+        configureUI()
+    }
+    
+    private func initSetting() {
+        folderCollectionView.delegate = self
+        folderCollectionView.dataSource = self
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = UIColor.gray50
+        view.addSubview(folderCollectionView)
+        folderCollectionView.register(FolderCell.self, forCellWithReuseIdentifier: "FolderCell")
+        
+        self.view.addSubview(self.emojiView)
+        self.emojiView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().inset(64)
+            make.leading.equalToSuperview().inset(24)
+            make.trailing.equalToSuperview().inset(24)
+            make.height.equalTo(126)
+        }
+        
+        self.emojiView.addSubview(self.countLabel)
+        self.countLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(24)
+            make.leading.equalToSuperview().inset(24)
+        }
+        
+        self.emojiView.addSubview(self.countExplainLabel)
+        self.countExplainLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(25)
+            make.leading.equalTo(countLabel.snp.trailing).offset(2)
+        }
+        
+        self.emojiView.addSubview(self.webExplainLabel)
+        self.webExplainLabel.snp.makeConstraints { make in
+            make.top.equalTo(countExplainLabel.snp.bottom).offset(3)
+            make.leading.equalToSuperview().inset(24)
+        }
+        
+        self.emojiView.addSubview(self.pasteImage)
+        pasteImage.snp.makeConstraints { make in
+            make.top.equalTo(webExplainLabel.snp.bottom).offset(16)
+            make.leading.equalToSuperview().inset(24)
+        }
+        
+        self.emojiView.addSubview(self.pasteLabel)
+        self.pasteLabel.snp.makeConstraints { make in
+            make.top.equalTo(webExplainLabel.snp.bottom).offset(16)
+            make.leading.equalToSuperview().inset(45)
+        }
+        
+        self.emojiView.addSubview(self.folderImage)
+        self.folderImage.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(47)
+            make.trailing.equalToSuperview().inset(30)
+        }
+        
+        self.emojiView.addSubview(self.pasteButton)
+        self.pasteButton.snp.makeConstraints { make in
+            make.top.equalTo(webExplainLabel.snp.bottom).offset(16)
+            make.leading.equalToSuperview().inset(24)
+            make.trailing.equalTo(folderImage.snp.leading).offset(140)
+        }
+        
+        pasteButton.rx.tap
+            .bind(onNext: {
+                UIPasteboard.general.string = self.webTempString
+            }).disposed(by: disposeBag)
+            
+        if let storedString = UIPasteboard.general.string {
+            print(storedString)
+        }
+
+        self.view.addSubview(self.folderCollectionView)
+        self.folderCollectionView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(emojiView.snp.bottom).offset(24)
+            make.leading.equalToSuperview().inset(24)
+            make.trailing.equalToSuperview().inset(24)
+            make.bottom.equalToSuperview().inset(24)
+        }
+    }
+}
+
+extension FolderViewController : UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return folderNameList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FolderCell.identifier,
+                                                            for: indexPath) as? FolderCell else {
+            return UICollectionViewCell()
+        }
+        cell.backgroundColor = .white
+        cell.layer.cornerRadius = 20
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemSpacing : CGFloat = 11
+        let myWidth : CGFloat = self.folderCollectionView.frame.width / 2 - itemSpacing
+        return CGSize(width: myWidth, height: myWidth * 0.917)
+     }
+}
