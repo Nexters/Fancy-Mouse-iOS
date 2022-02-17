8de6de10a1b303fc7eef9de1bff9f99300247c4e
Hansangjin98 <onetable98@naver.com>
2022-02-17 15:11:23 +0900

    [Feat] 단어 이동하기 뷰 UI구현

--- FancyMouse/Presentation/Voca/View/PopUpView/WordMoveView/WordMoveViewCell.swift
+++ FancyMouse/Presentation/Voca/View/PopUpView/WordMoveView/WordMoveViewCell.swift
@@ -5,4 +5,64 @@
 //  Created by 한상진 on 2022/02/17.
 //
 
-import Foundation
+import UIKit
+
+final class WordMoveViewCell: UITableViewCell {
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .spoqaRegular(size: 16)
+        label.textColor = .primaryColor
+        return label
+    }()
+    
+    private lazy var checkImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "check")?.withRenderingMode(.alwaysTemplate)
+        imageView.tintColor = .primaryColor
+        imageView.isHidden = true
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        wordMove()
+    }
+    
+    private func wordMove() {
+        //Todo
+    }
+    
+    private func setup() {
+        selectionStyle = .none
+        backgroundColor = .white
+        
+        addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.height.equalTo(20)
+            make.centerY.leading.equalToSuperview()
+        }
+        
+        addSubview(checkImageView)
+        checkImageView.snp.makeConstraints { make in
+            make.centerY.trailing.equalToSuperview()
+        }
+    }
+    
+    func setupLabel(_ title: String) {
+        titleLabel.text = title
+    }
+    
+    func setupSelected() {
+        titleLabel.font = .spoqaBold(size: 16)
+        checkImageView.isHidden = false
+    }
+}
