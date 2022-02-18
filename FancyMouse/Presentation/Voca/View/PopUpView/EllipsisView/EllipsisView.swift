26cb3c3c1226ce6e53a7af71d6014acc6181b57a
Hansangjin98 <onetable98@naver.com>
2022-02-19 02:20:34 +0900

    [Feat] Ellipsis 버튼 팝업 뷰 UI구현(구조변경)

--- FancyMouse/Presentation/Voca/View/PopUpView/EllipsisView/EllipsisView.swift
+++ FancyMouse/Presentation/Voca/View/PopUpView/EllipsisView/EllipsisView.swift
@@ -7,71 +7,49 @@
 
 import UIKit
 
-enum EllipsisMode {
-    case folder
-    case word
-}
-
 final class EllipsisView: UIView {
-    private lazy var editButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("수정하기", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = .spoqaMedium(size: 14)
-        button.setImage(UIImage(named: "edit"), for: .normal)
-        button.semanticContentAttribute = .forceLeftToRight
-        return button
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .fill
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.distribution = .fillEqually
+        return stackView
     }()
     
-    private lazy var deleteButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("삭제하기", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = .spoqaMedium(size: 14)
-        button.setImage(UIImage(named: "delete"), for: .normal)
-        button.semanticContentAttribute = .forceLeftToRight
-        return button
-    }()
-    
-    private lazy var moveButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("이동하기", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = .spoqaMedium(size: 14)
-        button.setImage(UIImage(named: "move"), for: .normal)
-        button.semanticContentAttribute = .forceLeftToRight
-        return button
-    }()
-    
-    init(frame: CGRect, mode: EllipsisMode) {
+    override init(frame: CGRect) {
         super.init(frame: frame)
-        setup(mode)
+        setup()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        setup()
     }
     
-    private func setup(_ mode: EllipsisMode) {
+    private func setup() {
         backgroundColor = .gray70
         layer.cornerRadius = 14
         
-        let button = mode == .folder ? moveButton : editButton
-        addSubview(button)
-        button.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(19)
-            make.bottom.equalToSuperview().offset(-57)
-            make.leading.equalToSuperview().offset(12)
-            make.trailing.equalToSuperview().inset(16)
-        }
-        
-        addSubview(deleteButton)
-        deleteButton.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(57)
-            make.bottom.equalToSuperview().offset(-19)
-            make.leading.equalToSuperview().offset(12)
-            make.trailing.equalToSuperview().inset(16)
+        addSubview(stackView)
+        stackView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.bottom.equalToSuperview().inset(16)
         }
     }
+    
+    func addComponent(title: String, imageName: String, action: UIAction) {
+        let button: UIButton = {
+            let button = UIButton()
+            let image = UIImage(named: imageName)
+            button.setTitle(title, for: .normal)
+            button.setTitleColor(.white, for: .normal)
+            button.titleLabel?.font = .spoqaMedium(size: 14)
+            button.setImage(image, for: .normal)
+            button.semanticContentAttribute = .forceLeftToRight
+            button.addAction(action, for: .touchUpInside)
+            return button
+        }()
+        stackView.addArrangedSubview(button)
+    }
 }
-
