a74cedb7fcb761adcf809ce21d6459a54f25013b
Lee424 <tmdghks424@naver.com>
2022-02-19 23:29:41 +0900

    만든사람들 뷰 추가

--- FancyMouse/Presentation/More/View/MakersView.swift
+++ FancyMouse/Presentation/More/View/MakersView.swift
@@ -5,4 +5,28 @@
 //  Created by seunghwan Lee on 2022/02/19.
 //
 
-import Foundation
+import SwiftUI
+
+struct MakersView: View {
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 40) {
+                Text("Fancymouse를\n만든 사람들")
+                    .spoqaRegular(size: 20)
+                    .foregroundColor(.white)
+                
+                Image("makers")
+                    .resizable()
+                    .scaledToFit()
+            }
+            .padding(EdgeInsets(top: 24, leading: 24, bottom: 50, trailing: 24))
+        }
+        .background(Color.primaryDark)
+    }
+}
+
+struct MakersView_Previews: PreviewProvider {
+    static var previews: some View {
+        MakersView()
+    }
+}
