ebde380b2e80f152f10de30072d17853ece366a8
suding <betty.sujin.lee@gmail.com>
2022-02-20 23:51:41 +0900

    [Add] ViewModelBindableType 생성

--- FancyMouse/Presentation/Folder/ViewModel/ViewModelBindableType.swift
+++ FancyMouse/Presentation/Folder/ViewModel/ViewModelBindableType.swift
@@ -9,5 +9,17 @@ import Foundation
 import UIKit
 
 protocol ViewModelBindableType {
+    associatedtype ViewModelType
+    
+    var viewModel: ViewModelType? { get set }
+    
+    func bindViewModel()
+}
 
+extension ViewModelBindableType where Self: UIViewController {
+    mutating func bind(viewModel: Self.ViewModelType) {
+        self.viewModel = viewModel
+        loadViewIfNeeded()
+        bindViewModel()
+    }
 }
