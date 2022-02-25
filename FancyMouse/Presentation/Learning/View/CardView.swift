799131aaaf82540575b1fdba8062be42534a709a
Lee424 <tmdghks424@naver.com>
2022-02-25 10:29:31 +0900

    학습 뷰, 기능 ing..

--- FancyMouse/Presentation/Learning/View/CardView.swift
+++ FancyMouse/Presentation/Learning/View/CardView.swift
@@ -12,7 +12,7 @@ struct WordMeaningRow: View {
     let meaning: String
     
     var body: some View {
-        HStack(spacing: 6) {
+        HStack(alignment: .top, spacing: 6) {
             Text(String(number) + ".")
                 .spoqaBold(size: 14)
                 .frame(alignment: .trailing)
@@ -62,7 +62,7 @@ struct CardView: View {
             
             if isVisiable {
                 // TODO: 유동적으로 데이터를 반영하기에는 list나 LazyVStack이 좋을듯, 데이터 구조 잡히면 변경할 예정, row 뷰도 별도로 뺄 예정
-                ScrollView {
+                ScrollView(showsIndicators: false) {
                     VStack(alignment: .leading, spacing: 6) {
                         
                         WordMeaningRow(number: 1, meaning: "(이루고자 하는, 이루어야 할) 목적")
@@ -71,16 +71,17 @@ struct CardView: View {
                         
                         WordMeaningRow(number: 3, meaning: "(특정 상황에서 무엇을) 하기 위함, 용도, 의도 하기 위함, 용도, 의도 하기 위함, 용도, 의도 하기 위함, 용도, 의도")
                     }
-                    .frame(maxWidth: .infinity)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
                 }
             } else {
                 Text("우측 하단의 아이콘을\n터치하면 뜻을 확인할 수 있어요!")
                     .spoqaRegular(size: 14)
                     .foregroundColor(.gray70)
                     .multilineTextAlignment(.center)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity,alignment: .top)
             }
             
-            Spacer(minLength: 40)
+//            Spacer(minLength: 40)
             
             HStack {
                 Spacer()
@@ -95,11 +96,109 @@ struct CardView: View {
         .padding(EdgeInsets(top: 24, leading: 24, bottom: 24, trailing: 24))
         .background(Color.white)
         .cornerRadius(30)
+        .shadow(
+            color: Color.gray50,
+            radius: 1,
+            x: 0,
+            y: 1
+        )
     }
 }
 
-struct CardView_Previews: PreviewProvider {
+struct CardStackView: View {
+//    var totalCardNum: Int = 200
+//    var currentCardNum: Int = 24
+//    var wordState: String = "암기중"// FIXME: 값 변경
+//    // Word 모델
+//    @State private var isVisiable: Bool = false
+    
+//    @State var offset: CGFloat = 0
+    @State private var translation: CGSize = .zero
+    
+    @State var endSwipe: Bool = false
+    @GestureState var isDragging: Bool = false
+
+    @EnvironmentObject var viewModel: CardViewModel
+    var card: CardTestModel
+    @Binding var testVal: CGFloat
+    
+    var body: some View {
+        GeometryReader { proxy in
+            CardView()
+            .rotationEffect(
+                .degrees(Double(self.translation.width / proxy.size.width * 20)),
+                anchor: .bottom
+            )
+            .offset(x: self.translation.width, y: self.translation.height)
+//            .contentShape(Rectangle().trim(from: 0, to: endSwipe ? 0 : 1))
+            .animation(.interactiveSpring(
+                response: 0.5,
+                blendDuration: 0.3)
+            )
+    //                                .offset(x: topOffset, y: offset)
+            .gesture(
+                DragGesture()
+                    .updating($isDragging, body: { value, out, _ in
+                        out = true
+                    })
+                    .onChanged({ value in
+                        self.translation = value.translation
+                        
+                        // Fix
+                        let width1 = (getRect().width - 50) / 2
+                        testVal = max((width1 - self.translation.width) / width1, 0.7)
+                        
+                        print(self.translation.width, width1, testVal)
+    //                                            let translation = value.translation.width
+//                        offset = (isDragging ? self.translation.width : .zero)
+                    })
+                    .onEnded({ value in
+                        let width = getRect().width - 50
+//                        let translation = value.translation.width
+                        self.translation = value.translation
+                        
+                        // FIX
+//                        let width1 = (getRect().width - 50) / 2
+//                        testVal = max((width1 - self.translation.width) / width1, 0)
+//                        testVal = 1
+
+                        let checkingStatus = (self.translation.width > 0 ? self.translation.width : -self.translation.width)
+    //                                endSwipe = true
+                        withAnimation {
+                            if checkingStatus > (width / 2) {
+                                // remoce card
+                                self.translation.width = (self.translation.width > 0 ? width : -width) * 2
+                                endSwipe = true
+                                endSwipeActions()
+                            } else {
+                                // reset
+//                                offset = .zero
+                                testVal = 1
+                                self.translation = .zero
+                            }
+                        }
+                    })
+            )
+        }
+    }
+    
+    func endSwipeActions() {
+        withAnimation(.none) {
+            endSwipe = true
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            if let _ = viewModel.dummyData.first {
+                let _ = withAnimation {
+                    viewModel.dummyData.removeFirst()
+                }
+            }
+        }
+    }
+}
+
+struct CardStackView_Previews: PreviewProvider {
     static var previews: some View {
-        CardView()
+        CardStackView(card: CardTestModel(word: "1"), testVal: .constant(1))
     }
 }
--- FancyMouse/Presentation/Learning/View/LearningView.swift
+++ FancyMouse/Presentation/Learning/View/LearningView.swift
@@ -69,23 +69,114 @@ struct CompleteButton: View {
     }
 }
 
+struct CardTestModel: Identifiable {
+    let id = UUID()
+    let word: String
+}
+
+class LearningViewModel: ObservableObject {
+    @Published var words: [Word] = []
+    
+    init() {
+        fetchDummyData()
+    }
+    
+    private func fetchDummyData() {
+        words = Array(repeating: Word(wordID: 0, folderID: 0, createdAt: Date(), spelling: "Purpose", meanings: ["(이루고자 하는, 이루어야 할) 목적,(특정 상황에서 무엇을) 하기 위함.","(삶에 의미를 주는) 목적"], memorizationStatus: .inProgress, memo: "", synonyms: [""], examples: [""], urlString: ""), count: 10)
+    }
+    
+    //TODO: 페이징 처리, 단어가 5개만 남았을 때, 한번에 10개씩 불러오기
+    
+    // 암기 완료 버튼 누르거나 우측으로 스와이프 시 해당 단어 스테이터스 암기 완료로 변경 및 반영
+    
+}
+
+class CardViewModel: ObservableObject {
+    @Published var dummyData: [CardTestModel] =
+    [CardTestModel(word: "1"), CardTestModel(word: "1"), CardTestModel(word: "1"), CardTestModel(word: "1"), CardTestModel(word: "1"), CardTestModel(word: "1"), CardTestModel(word: "1")]
+    
+    func getIndex(dummy: CardTestModel) -> Int {
+        let index = dummyData.firstIndex { card in
+            return dummy.id == card.id
+        } ?? 0
+        
+        return index
+    }
+}
+
+struct CardContainerView: View {
+//    @ObservedObject var viewModel = CardViewModel()
+    @StateObject var viewModel = CardViewModel()
+    
+    @State var offset: CGFloat = 0
+    
+    @State private var translation: CGSize = .zero
+    
+    @State var endSwipe: Bool = false
+    @GestureState var isDragging: Bool = false
+    
+    @State var testVal: CGFloat = 1
+    
+    var body: some View {
+        ZStack {
+            if viewModel.dummyData.isEmpty {
+                //FIXME: 임시
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            } else {
+                ForEach(viewModel.dummyData.reversed()) { card in
+                    let index = CGFloat(viewModel.getIndex(dummy: card))
+                    let topOffset = (index <= 2 ? index : 2) * 10 * testVal
+                    
+                    GeometryReader { proxy in
+                        let size = proxy.size
+                        
+                        ZStack {
+                            // 카드뷰 자체가 오프셋을 갖고..
+                            // 프레임하고 y오프셋 조절
+                            // 백그라운드 색 조절 필요
+                            CardStackView(card: card, testVal: $testVal)
+                                .environmentObject(viewModel)
+                                .frame(width: size.width - (topOffset * 4), height: size.height)
+                                .offset(y: topOffset)
+                                .offset(x: offset)
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension View {
+    func getRect() -> CGRect {
+        return UIScreen.main.bounds
+    }
+}
+
 struct LearningView: View {
     @State private var checkIsTapped = false
     @State private var xmarkIsTapped = false
     
+    let rectWidth = UIScreen.main.bounds.width
+    let rectHeight = UIScreen.main.bounds.height
+    
     var body: some View {
         VStack(alignment: .center, spacing: 0) {
-            // TODO: 카드뷰 개발 중, 개발 완료후 교체 예정
-            
-            CardView()
-                .padding(EdgeInsets(top: 40, leading: 24, bottom: 60, trailing: 24))
-            
-            HStack {
-                CompleteButton(isTapped: $checkIsTapped)
-                Spacer()
-                IncompleteButton(isTapped: $xmarkIsTapped)
+            VStack(alignment: .center, spacing: 0) {
+                CardContainerView()
+                    .padding(EdgeInsets(top: rectHeight * 0.091, leading: 0, bottom: rectHeight * 0.137, trailing: 0))
+                
+                HStack {
+                    CompleteButton(isTapped: $checkIsTapped)
+                    Spacer()
+                    IncompleteButton(isTapped: $xmarkIsTapped)
+                }
+                .padding(EdgeInsets(top: 0, leading: 36, bottom: rectHeight * 0.091, trailing: 36))
             }
-            .padding(EdgeInsets(top: 0, leading: 60, bottom: 24, trailing: 60))
+            .padding(.horizontal, rectWidth * 0.073)
         }
         .background(Color.primaryDark)
     }
