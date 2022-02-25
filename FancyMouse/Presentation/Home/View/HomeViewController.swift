24a0e575d5269a2068429bf153464bb15deb5a66
yeonduing <yeonduing@gmail.com>
2022-02-26 03:38:23 +0900

    HomeSectionHeaderView 생성 및 적용

--- FancyMouse/Presentation/Home/View/HomeViewController.swift
+++ FancyMouse/Presentation/Home/View/HomeViewController.swift
@@ -19,6 +19,8 @@ final class HomeViewController: UIViewController {
         let tableView = UITableView(frame: .zero, style: .grouped)
         tableView.backgroundColor = .clear
         tableView.separatorStyle = .none
+        tableView.scrollIndicatorInsets = UIEdgeInsets(top: 305, left: 0, bottom: 0, right: -10)
+        
         tableView.register(
             HomeWordCell.self,
             forCellReuseIdentifier: "HomeWordCell"
@@ -26,6 +28,11 @@ final class HomeViewController: UIViewController {
         tableView.register(
             HomeProgressView.self,
             forHeaderFooterViewReuseIdentifier: "HomeProgressView"
+        )
+        
+        tableView.register(
+            HomeSectionHeaderView.self,
+            forHeaderFooterViewReuseIdentifier: "HomeSectionHeaderView"
         )
         
         return tableView
@@ -60,14 +67,18 @@ private extension HomeViewController {
     }
     
     func setupMockData() {
-        for idx in (0...4) {
-            let spellings = ["ABCD", "ZBBD", "EBCD", "DEFQ", "DDDD"]
-            let dates = [Date(timeIntervalSinceNow: -1000),
-                         Date(timeIntervalSinceNow: -10),
-                         Date(timeIntervalSinceNow: -30050),
-                         Date(timeIntervalSinceNow: -3040500),
-                         Date(timeIntervalSinceNow: -30)]
-            let word = Word(id: idx, folderID: 0, createdAt: dates[idx], spelling: spellings[idx], meanings: ["sadf", "sdfjksd"], memorizationStatus: .incomplete, memo: nil, synonyms: ["sdf"], examples: ["sdf"], urlString: "dsf")
+        let spellings = ["comprehensive", "strategy", "complication", "dim", "access", "resource", "sentimental"]
+        let meaningsList = [["포괄적인", "종합적인", "능력별 구분을 않는"], ["전략", "계획"], ["(상황을 더 복잡하게 만드는) 문제", "복잡함"], ["(빛이) 어둑한", "(장소가) 어둑한", "(형체가) 흐릿한"]
+                            ,["(장소로의) 입장", "접근권, 접촉기회", "(컴퓨터에) 접속하다"], ["(자원, 재원", "원하는 목적을 이루는 데 도움이 되는) 재료[자산]", "자원[재원]을 제공하다"],
+                            ["정서(감정)적인", "(지나치게) 감상적인"]]
+        let memos = ["이건 제발 외우자! ㅜㅜ", "", "", "디자인 관련해서 자주 나오는 용어!", "", "", ""]
+        let synonymsList: [[String]] = [["complete", "full"],[],[],["vague"],[],[],[]]
+        let examplesList: [[String]] = [["a comprehensive survey of modern music."],[],[],["This light is too dim to read by."],[],[],[]]
+        
+        for idx in (0..<spellings.count) {
+            let word = Word(id: idx, folderID: idx, createdAt: Date(timeIntervalSinceNow: Double(arc4random_uniform(100000))),
+                            spelling: spellings[idx], meanings: meaningsList[idx],
+                            memorizationStatus: .inProgress, memo: memos[idx], synonyms: synonymsList[idx], examples: examplesList[idx], urlString: "")
             words.append(word)
         }
     }
@@ -83,6 +94,19 @@ private extension HomeViewController {
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return words.count
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let headerView = tableView.dequeueReusableHeaderFooterView(
+            withIdentifier: "HomeSectionHeaderView"
+        ) as? HomeSectionHeaderView
+        else { return nil }
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        50
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -128,5 +152,69 @@ struct HomeViewUseCase: HomeUseCaseProtocol {
     
     func loadWords() -> Observable<[Word]> {
         return PublishSubject<[Word]>()
+    }
+}
+
+final class HomeSectionHeaderView: UITableViewHeaderFooterView {
+    private var hiddingLabel: UILabel {
+        let hiddingLabel = UILabel()
+        hiddingLabel.textColor = .gray50
+        hiddingLabel.font = .spoqaRegular(size: 14)
+        
+        return hiddingLabel
+    }
+    
+    private lazy var sectionHeaderView: UIView = {
+        let sectionHeaderView = UIView()
+        
+        let hidingSpellingLabel = hiddingLabel
+        let ellipseImageView = UIImageView()
+        let hidingMeaningsLabel = hiddingLabel
+        let suffleButton = UIButton()
+        
+        hidingSpellingLabel.text = "단어숨김"
+        hidingMeaningsLabel.text = "뜻숨김"
+        ellipseImageView.image = #imageLiteral(resourceName: "ellipse")
+        suffleButton.setImage(#imageLiteral(resourceName: "suffle"), for: .normal)
+        
+        [hidingSpellingLabel, ellipseImageView, hidingMeaningsLabel, suffleButton].forEach {
+            sectionHeaderView.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            hidingSpellingLabel.centerYAnchor.constraint(equalTo: suffleButton.centerYAnchor),
+            hidingSpellingLabel.leadingAnchor.constraint(equalTo: sectionHeaderView.leadingAnchor),
+            
+            ellipseImageView.centerYAnchor.constraint(equalTo: suffleButton.centerYAnchor),
+            ellipseImageView.leadingAnchor.constraint(equalTo: hidingSpellingLabel.trailingAnchor, constant: 8),
+            
+            hidingMeaningsLabel.centerYAnchor.constraint(equalTo: suffleButton.centerYAnchor),
+            hidingMeaningsLabel.leadingAnchor.constraint(equalTo: ellipseImageView.trailingAnchor, constant: 8),
+            
+            suffleButton.topAnchor.constraint(equalTo: sectionHeaderView.topAnchor),
+            suffleButton.bottomAnchor.constraint(equalTo: sectionHeaderView.bottomAnchor),
+            suffleButton.trailingAnchor.constraint(equalTo: sectionHeaderView.trailingAnchor)
+        ])
+        
+        return sectionHeaderView
+    }()
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        addSubview(sectionHeaderView)
+        sectionHeaderView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            sectionHeaderView.topAnchor.constraint(equalTo: topAnchor),
+            sectionHeaderView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            sectionHeaderView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            sectionHeaderView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
