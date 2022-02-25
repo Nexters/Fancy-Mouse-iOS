6bcbeb413d69717267db7c00fab248385bd509ae
sujin <betty.sujin.lee@gmail.com>
2022-02-25 18:29:10 +0900

    Walkthrough 파일 추가

---
+++ FancyMouse/Presentation/Walkthrough/View/WalkthroughFirstViewController.swift
@@ -0,0 +1,8 @@
+//
+//  WalkthroughFirstViewController.swift
+//  FancyMouse
+//
+//  Created by suding on 2022/02/25.
+//
+
+import Foundation
---
+++ FancyMouse/Presentation/Walkthrough/View/WalkthroughThirdViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WalkthroughThirdViewController.swift
+//  FancyMouse
+//
+//  Created by suding on 2022/02/25.
+//
+
+import UIKit
+
+class WalkthroughThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
