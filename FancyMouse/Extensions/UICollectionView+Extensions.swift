060622bde719035cc41499c1ef1a18b3b4e366c5
yeonduing <yeonduing@gmail.com>
2022-03-01 00:30:01 +0900

    UICollectionView register 및 dequeue 메소드 확장

--- FancyMouse/Extensions/UICollectionView+Extensions.swift
+++ FancyMouse/Extensions/UICollectionView+Extensions.swift
@@ -5,4 +5,61 @@
 //  Created by itzel.du on 2022/02/28.
 //
 
-import Foundation
+import UIKit
+
+extension UICollectionView {
+    func registerCell(
+        ofType cellType: UICollectionViewCell.Type,
+        withReuseIdentifier identifier: String? = nil
+    ) {
+        let reuseIdentifier = identifier ?? String(describing: cellType.self)
+        register(cellType, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+    
+    func dequeueCell<T: UICollectionViewCell>(
+        withReuseIdentifier reuseIdentifier: String? = nil,
+        for indexPath: IndexPath
+    ) -> T {
+        let reuseIdentifier = reuseIdentifier ?? String(describing: T.self)
+        
+        guard let cell = dequeueReusableCell(
+            withReuseIdentifier: reuseIdentifier,
+            for: indexPath
+        ) as? T else {
+            fatalError()
+        }
+        
+        return cell
+    }
+    
+    func registerSupplementaryView(
+        ofType viewType: UICollectionReusableView.Type,
+        forSupplementaryViewOfKind elementKind: String = UICollectionView.elementKindSectionHeader,
+        withReuseIdentifier identifier: String? = nil
+    ) {
+        let reuseIdentifier = identifier ?? String(describing: viewType.self)
+        register(
+            viewType,
+            forSupplementaryViewOfKind: elementKind,
+            withReuseIdentifier: reuseIdentifier
+        )
+    }
+    
+    func dequeueSupplementaryView<T: UICollectionReusableView>(
+        ofKind elementKind: String = UICollectionView.elementKindSectionHeader,
+        reuseIdentifier: String? = nil,
+        indexPath: IndexPath
+    ) -> T {
+        let reuseIdentifier = reuseIdentifier ?? String(describing: T.self)
+        
+        guard let view = self.dequeueReusableSupplementaryView(
+            ofKind: elementKind,
+            withReuseIdentifier: reuseIdentifier,
+            for: indexPath
+        ) as? T else {
+            fatalError()
+        }
+        
+        return view
+    }
+}
