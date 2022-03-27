5d252eb0b5a751f2505437edd1be202cb35e089d
Lee424 <tmdghks424@naver.com>
2022-03-27 19:56:06 +0900

    [Update] BaseViewController

--- FancyMouse/Common/BaseViewController.swift
+++ FancyMouse/Common/BaseViewController.swift
@@ -5,4 +5,15 @@
 //  Created by seunghwan Lee on 2022/03/27.
 //
 
-import Foundation
+import UIKit
+
+final class BaseViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        navigationController?.navigationBar.tintColor = .primaryColor
+    }
+}
