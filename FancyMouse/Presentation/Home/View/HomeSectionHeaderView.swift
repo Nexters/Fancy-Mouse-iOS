21f8d656960905afb193a566bdaf1f9831347d6c
yeonduing <yeonduing@gmail.com>
2022-04-14 02:10:34 +0900

    HomeSectionHeaderView delegate 적용
    - label -> button
    - tap action delegate 적용

--- FancyMouse/Presentation/Home/View/HomeSectionHeaderView.swift
+++ FancyMouse/Presentation/Home/View/HomeSectionHeaderView.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import RxSwift
+
 protocol HomeSectionHeaderViewDelegate: AnyObject {
     func didTapShuffleButton(_ homeSectionHeaderView: HomeSectionHeaderView)
     func didTapHidingSpellingButton(_ homeSectionHeaderView: HomeSectionHeaderView)
@@ -13,74 +15,91 @@ protocol HomeSectionHeaderViewDelegate: AnyObject {
 }
 
 final class EmptySectionHeaderView: UICollectionReusableView { }
+
 final class HomeSectionHeaderView: UICollectionReusableView {
     private let shuffleButton = UIButton()
-    private let hidingSpellingLabel = HomeWordHidingLabel()
+    private let hidingSpellingButton = HomeWordHidingButton()
     private let ellipseImageView = UIImageView()
-    private let hidingMeaningsLabel = HomeWordHidingLabel()
+    // TODO: 영어천재 sueaty가 meaning 대신 definition을 쓰라고 했음
+    private let hidingMeaningsButton = HomeWordHidingButton()
+    private let disposeBag = DisposeBag()
+    
+    weak var delegate: HomeSectionHeaderViewDelegate?
+    var hidingStatusObservable: Observable<HomeViewModel.HidingStatus>?
     
     override init(frame: CGRect) {
         super.init(frame: .zero)
         
         setupUI()
         setupLayout()
+        setupAction()
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    func addActionToSuffleButton(
-        _ action: UIAction,
-        for controlEvent: UIControl.Event = .touchUpInside
-    ) {
-        shuffleButton.addAction(action, for: controlEvent)
     }
 }
 
 private extension HomeSectionHeaderView {
     func setupUI() {
         backgroundColor = .gray30
-        hidingSpellingLabel.text = "단어숨김"
-        hidingMeaningsLabel.text = "뜻숨김"
+        hidingSpellingButton.setTitle("단어숨김", for: .normal)
+        hidingMeaningsButton.setTitle("뜻숨김", for: .normal)
         ellipseImageView.image = #imageLiteral(resourceName: "ellipse")
         shuffleButton.setImage(#imageLiteral(resourceName: "shuffle"), for: .normal)
     }
     
+    func setupAction() {
+        let hidingSpellingButtonAction = UIAction { _ in
+            self.delegate?.didTapHidingSpellingButton(self)
+        }
+        hidingSpellingButton.addAction(hidingSpellingButtonAction, for: .touchUpInside)
+        
+        let hidingMeaningsButtonAction = UIAction { _ in
+            self.delegate?.didTapHidingMeaningsButton(self)
+        }
+        hidingMeaningsButton.addAction(hidingMeaningsButtonAction, for: .touchUpInside)
+        
+        let shuffleButtonAction = UIAction { _ in
+            self.delegate?.didTapShuffleButton(self)
+        }
+        shuffleButton.addAction(shuffleButtonAction, for: .touchUpInside)
+    }
+    
     func setupLayout() {
-        [hidingSpellingLabel, ellipseImageView, hidingMeaningsLabel, shuffleButton].forEach {
+        [hidingSpellingButton, ellipseImageView, hidingMeaningsButton, shuffleButton].forEach {
             addSubview($0)
         }
         
-        hidingSpellingLabel.snp.makeConstraints { make in
+        hidingSpellingButton.snp.makeConstraints { make in
             make.centerY.equalTo(shuffleButton.snp.centerY)
-            make.leading.equalToSuperview()
+            make.leading.equalToSuperview().offset(24)
         }
         
         ellipseImageView.snp.makeConstraints { make in
             make.centerY.equalTo(shuffleButton.snp.centerY)
-            make.leading.equalTo(hidingSpellingLabel.snp.trailing).offset(8)
+            make.leading.equalTo(hidingSpellingButton.snp.trailing).offset(8)
         }
         
-        hidingMeaningsLabel.snp.makeConstraints { make in
+        hidingMeaningsButton.snp.makeConstraints { make in
             make.centerY.equalTo(shuffleButton.snp.centerY)
             make.leading.equalTo(ellipseImageView.snp.trailing).offset(8)
         }
         
         shuffleButton.snp.makeConstraints { make in
-            make.height.equalTo(18)
             make.top.bottom.equalToSuperview().offset(3)
-            make.trailing.equalToSuperview()
+            make.trailing.equalToSuperview().inset(24)
         }
     }
 }
 
-final class HomeWordHidingLabel: UILabel {
+final class HomeWordHidingButton: UIButton {
     init() {
         super.init(frame: .zero)
         
-        textColor = .gray50
-        font = .spoqaRegular(size: 14)
+        setTitleColor(.gray50, for: .normal)
+        titleLabel?.font = .spoqaRegular(size: 14)
     }
     
     required init?(coder: NSCoder) {
