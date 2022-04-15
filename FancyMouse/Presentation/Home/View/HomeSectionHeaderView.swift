c49105f90105aa3b491eaaec4b9fe751e0604d52
yeonduing <yeonduing@gmail.com>
2022-04-15 09:32:04 +0900

    WordSectionHeaderView 생성

--- FancyMouse/Presentation/Home/View/HomeSectionHeaderView.swift
+++ FancyMouse/Presentation/Home/View/HomeSectionHeaderView.swift
@@ -5,8 +5,8 @@
 //  Created by itzel.du on 2022/03/22.
 //
 
-import UIKit
 import RxSwift
+import UIKit
 
 protocol HomeSectionHeaderViewDelegate: AnyObject {
     func didTapShuffleButton(_ homeSectionHeaderView: HomeSectionHeaderView)
@@ -15,6 +15,74 @@ protocol HomeSectionHeaderViewDelegate: AnyObject {
 }
 
 final class EmptySectionHeaderView: UICollectionReusableView { }
+
+final class WordSectionHeaderView: UICollectionReusableView {
+    private let totalLabel = UILabel()
+    private let numberLabel = UILabel()
+    private let countLabel = UILabel()
+    private let filterButton = UIButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        setupUI()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    var totalCount: Int = 0 {
+        didSet {
+            numberLabel.text = "\(totalCount)"
+        }
+    }
+}
+
+private extension WordSectionHeaderView {
+    func setupUI() {
+        totalLabel.text = "총"
+        numberLabel.text = "\(totalCount)"
+        countLabel.text = "개"
+        
+        [totalLabel, countLabel].forEach {
+            $0.font = .spoqaRegular(size: 14)
+            $0.textColor = .gray50
+        }
+        
+        numberLabel.font = .spoqaBold(size: 14)
+        numberLabel.textColor = .gray70
+        
+        filterButton.setImage(#imageLiteral(resourceName: "btn_filter"), for: .normal)
+    }
+    
+    func setupLayout() {
+        [totalLabel, numberLabel, countLabel, filterButton].forEach {
+            addSubview($0)
+        }
+        
+        totalLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(filterButton.snp.centerY)
+            make.leading.equalToSuperview().offset(24)
+        }
+        
+        numberLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(filterButton.snp.centerY)
+            make.leading.equalTo(totalLabel.snp.trailing).offset(3)
+        }
+        
+        countLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(filterButton.snp.centerY)
+            make.leading.equalTo(numberLabel.snp.trailing)
+        }
+        
+        filterButton.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.trailing.equalToSuperview().inset(24)
+        }
+    }
+}
 
 final class HomeSectionHeaderView: UICollectionReusableView {
     private let shuffleButton = UIButton()
